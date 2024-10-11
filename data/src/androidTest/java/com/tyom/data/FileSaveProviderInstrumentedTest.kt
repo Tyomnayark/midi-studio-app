e85d480930cc922e486a8dc4e661fb0b9c83352e
Elena <tyomnayark@gmail.com>
2024-10-11 23:42:50 +0500

    test

--- data/src/androidTest/java/com/tyom/data/FileSaveProviderInstrumentedTest.kt
+++ data/src/androidTest/java/com/tyom/data/FileSaveProviderInstrumentedTest.kt
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class FileSaveProviderInstrumentedTest {
-    
+
     private lateinit var context: Context
     private lateinit var fileSaveProvider: FileSaveProvider
     private lateinit var noteListConfiguration: NoteListConfiguration
