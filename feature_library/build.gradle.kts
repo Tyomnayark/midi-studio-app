7a702c877c952baf2d56b343e7cad66e35b13eb2
tyomnayark <tyomnayark@gmail.com>
2024-10-13 01:52:05 +0500

    delete stages

--- feature_library/build.gradle.kts
+++ feature_library/build.gradle.kts
@@ -29,7 +29,9 @@ android {
         jvmTarget = "1.8"
     }
     packagingOptions {
-        exclude 'META-INF/gradle/incremental.annotation.processors'
+        resources {
+            excludes += setOf("META-INF/gradle/incremental.annotation.processors")
+        }
     }
 }
 
