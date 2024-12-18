f4d0e8cc40ea69efe4e35f382fdfd1265c226114
Elena <tyomnayark@gmail.com>
2024-12-18 20:29:27 +0500

    update git

--- app/src/main/java/com/tyom/notestudio/App.kt
+++ app/src/main/java/com/tyom/notestudio/App.kt
@@ -1,10 +1,7 @@
 package com.tyom.notestudio
 
 import android.app.Application
-import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.analytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
